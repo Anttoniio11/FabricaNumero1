31f8caeb552d6d097d608633452a72e09cd2caae
Anttoniio11 <anttoniio2312@gmail.com>
2024-10-07 11:37:06 -0500

    Primer api

--- app/Models/Transaccion.php
+++ app/Models/Transaccion.php
@@ -4,13 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Transaccion extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['monto', 'motivo', 'fecha'];
-    // , 'id_user', 'id_tipo_transaccion'
+    protected $fillable = [
+        'monto', 'motivo', 'fecha', 'id_tipo_transaccion',
+    ];
+
+     // Relaciones permitidas para incluir
+     protected $allowIncluded = ['tipoTransaccion', 'user' ];
+
+     // Campos permitidos para filtrado
+     protected $allowFilter = [
+        'monto', 'motivo', 'fecha',
+     ];
+
+     // Campos permitidos para ordenamiento
+     protected $allowSort = [
+        'monto', 'motivo', 'fecha',
+     ];
 
 
     public function tipoTransaccion(){
@@ -20,5 +35,78 @@ class Transaccion extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+
+     // Scope para incluir relaciones
+    public function scopeIncluded(Builder $query)
+    {
+        if (empty($this->allowIncluded) || empty(request('included'))) {
+            return;
+        }
+
+        $relations = explode(',', request('included'));
+        $allowIncluded = collect($this->allowIncluded);
+
+        foreach ($relations as $key => $relationship) {
+            if (!$allowIncluded->contains($relationship)) {
+                unset($relations[$key]);
+            }
+        }
+        $query->with($relations);
+    }
+
+    // Scope para filtrar resultados
+    public function scopeFilter(Builder $query)
+    {
+        if (empty($this->allowFilter) || empty(request('filter'))) {
+            return;
+        }
+
+        $filters = request('filter');
+        $allowFilter = collect($this->allowFilter);
+
+        foreach ($filters as $filter => $value) {
+            if ($allowFilter->contains($filter)) {
+                $query->where($filter, 'LIKE', '%' . $value . '%');
+            }
+        }
+    }
+
+    // Scope para ordenar resultados
+    public function scopeSort(Builder $query)
+    {
+        if (empty($this->allowSort) || empty(request('sort'))) {
+            return;
+        }
+
+        $sortFields = explode(',', request('sort'));
+        $allowSort = collect($this->allowSort);
+
+        foreach ($sortFields as $sortField) {
+            $direction = 'asc';
+
+            if (substr($sortField, 0, 1) == '-') {
+                $direction = 'desc';
+                $sortField = substr($sortField, 1);
+            }
+            if ($allowSort->contains($sortField)) {
+                $query->orderBy($sortField, $direction);
+            }
+        }
+    }
+
+    // Scope para obtener o paginar resultados
+    public function scopeGetOrPaginate(Builder $query)
+    {
+        if (request('perPage')) {
+            $perPage = intval(request('perPage'));
+
+            if ($perPage) {
+                return $query->paginate($perPage);
+            }
+        }
+        return $query->get();
+    }
+
 
 }
